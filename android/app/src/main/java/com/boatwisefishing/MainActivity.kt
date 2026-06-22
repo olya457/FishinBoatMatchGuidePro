39863e77ba24f59f04864bad673b959444ad6a2b
olya <olya0665643972@gmail.com>
2026-06-22 11:38:09 +0300

    init

--- android/app/src/main/java/com/boatwisefishing/MainActivity.kt
+++ android/app/src/main/java/com/boatwisefishing/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.fishingboathandbook
+package com.boatwisefishing
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/boatwisefishing/MainApplication.kt
+++ android/app/src/main/java/com/boatwisefishing/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.fishingboathandbook
+package com.boatwisefishing
 
 import android.app.Application
 import com.facebook.react.PackageList
